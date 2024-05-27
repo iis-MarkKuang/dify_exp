35aaf5aece0a1c4cb8c77707c3cbbbdd9f6f38fa
Uozumi.Jun <1140387970@qq.com>
2024-05-27 14:43:12 +0800

    Dep error fix.

--- web/app/components/workflow/nodes/doc-select/panel.tsx
+++ web/app/components/workflow/nodes/doc-select/panel.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import useConfig from './use-config'
 import AddDoc from './components/add-dataset'
+import DocumentsV2List from "./components/dataset-list";
 import type { DocSelectNodeType } from './types'
 import Field from '@/app/components/workflow/nodes/_base/components/field'
 import Split from '@/app/components/workflow/nodes/_base/components/split'
