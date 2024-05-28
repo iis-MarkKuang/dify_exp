4872ca889cefd0d43c6c60fe4d8b2d1be064eb19
Uozumi.Jun <1140387970@qq.com>
2024-05-28 09:18:49 +0800

    Translation api.

---
+++ api/controllers/console/app/doc_translation.py
@@ -0,0 +1,19 @@
+import logging
+
+from flask_restful import Resource, fields, marshal_with, reqparse
+from werkzeug.datastructures import FileStorage
+from controllers.console import api
+
+
+class DocumentTranslationApi(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('file', type=FileStorage, location='files')
+        args = parser.parse_args()
+        pdf_file = args['file']
+        print('got file')
+        print(pdf_file)
+        print(pdf_file.tell())
+
+
+api.add_resource(DocumentTranslationApi, '/api/document/translate')
