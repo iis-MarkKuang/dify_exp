96e06f8134eb751cc8eb2cc81f0c4f1463c3af29
Uozumi.Jun <1140387970@qq.com>
2024-05-28 15:13:16 +0800

    FileVar.

--- api/controllers/console/doc_translation.py
+++ api/controllers/console/doc_translation.py
@@ -8,16 +8,15 @@ from controllers.console import api
 
 class DocumentTranslationApi(Resource):
     def post(self):
-        print(dict(request.form))
-        print('form ' + request.form)
-        print('data ' + request.data)
-        parser = reqparse.RequestParser()
-        parser.add_argument('file', type=FileVar, location='form')
-        args = parser.parse_args()
-        print(args)
-        pdf_file = args['file']
+        file = dict(request.form)['file']
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('file', type=FileVar, location='form')
+        # args = parser.parse_args()
+        # print(args)
+        # pdf_file = args['file']
         print('got file')
-        print(pdf_file)
+        print(file)
+        print(type(file))
 
 
 api.add_resource(DocumentTranslationApi, '/document/translate')
