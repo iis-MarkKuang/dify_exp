78b43cc74c9fd880f694dc579a6f4942986831da
Uozumi.Jun <1140387970@qq.com>
2024-06-03 14:41:44 +0800

    fix.

--- api/controllers/console/__init__.py
+++ api/controllers/console/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 from libs.external_api import ExternalApi
 from prometheus_flask_exporter import RESTfulPrometheusMetrics
-from ..app import app as dify_app
+from ...app import app as dify_app
 
 bp = Blueprint('console', __name__, url_prefix='/console/api')
 api = ExternalApi(bp)
