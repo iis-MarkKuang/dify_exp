76ff6ae79b754d8e5092a72c3085343e962ae2e5
Uozumi.Jun <1140387970@qq.com>
2024-06-03 16:49:18 +0800

    import issue.

--- api/controllers/console/__init__.py
+++ api/controllers/console/__init__.py
@@ -4,19 +4,19 @@ from flask import Blueprint
 from libs.external_api import ExternalApi
 from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
-def _create_response_converter(api):
+def get_custom_resp_converter():
     def _make_response(response):
         print('OWN RESP')
         print(response)
         if response is None:
             response = (None, 200)
-        return (json.loads(response), 200)
+        return (json.loads(json.dumps(response)), 200)
 
     return _make_response
 
 bp = Blueprint('console', __name__, url_prefix='/console/api')
 api = ExternalApi(bp)
-metrics = RESTfulPrometheusMetrics(app=None, api=api, response_converter=_create_response_converter(api))
+metrics = RESTfulPrometheusMetrics(app=None, api=api, response_converter=get_custom_resp_converter())
 
 
 # Import other controllers
