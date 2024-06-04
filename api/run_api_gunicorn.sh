7c4272e939e42c0f79701d528920cf285a0c714b
microware <microware@microwaredeMacBook-Pro.local>
2024-06-04 14:14:35 +0800

    rm redundant

--- api/run_api_gunicorn.sh
+++
@@ -1,5 +0,0 @@
-export DIFY_HOME="/home/ykuang/dify_exp"
-source "$DIFY_HOME/api/env_act.sh"
-cd "$DIFY_HOME/api"
-gunicorn -w 4 'app:app' -b:5001
-
--- web/run_web.sh
+++
@@ -1,6 +0,0 @@
-cd /home/ykuang/dify_exp/web
-# source load_nvm.sh
-export NVM_DIR="/home/ykuang/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm use 18
-npm run dev
